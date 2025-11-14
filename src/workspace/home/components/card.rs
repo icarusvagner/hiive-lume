0c3b3f80273d4c5299cfebcf17d29ba143939c42
icarusvagner <icarusvagner@gmail.com>
2025-11-14 11:13:05 +0800

    initial commit

--- src/workspace/home/components/card.rs
+++ src/workspace/home/components/card.rs
@@ -1,5 +1,15 @@
 use gpui::*;
+use gpui_component::v_flex;
 
-pub fn card() -> impl IntoElement {
-    div()
+pub fn custom_card(child: impl IntoElement, bg: Hsla) -> Div {
+    v_flex()
+        .rounded_xl()
+        .shadow_2xl()
+        .border_1()
+        .border_color(black().opacity(0.40))
+        .bg(bg)
+        .p_12()
+        .w_full()
+        .h_full()
+        .child(child)
 }
--- src/workspace/home/dashboard/content.rs
+++ src/workspace/home/dashboard/content.rs
@@ -17,6 +17,7 @@ use crate::{
         dashboard_card_data::{DashboardCardData, DashboardPieCard},
         events_news_data::EventsNewsData,
     },
+    workspace::home::components::card::custom_card,
 };
 
 #[derive(Clone)]
@@ -42,22 +43,21 @@ impl DashboardContent {
         let mut cards = Vec::new();
 
         for card in cards_data {
-            let card = v_flex()
-                .rounded_xl()
-                .shadow_2xl()
-                .border_1()
-                .border_color(black().opacity(0.40))
-                .bg(cx.theme().secondary)
-                .p_12()
-                .w_full()
-                .child(
-                    Avatar::new()
-                        .p_4()
-                        .placeholder(card.icon().text_color(cx.theme().foreground))
-                        .bg(cx.theme().blue.opacity(0.40)),
-                )
-                .child(div().child(card.label()).text_sm())
-                .child(div().child(card.content()).text_2xl().font_semibold());
+            let card = custom_card(
+                v_flex()
+                    .w_full()
+                    .items_center()
+                    .justify_start()
+                    .child(
+                        Avatar::new()
+                            .p_4()
+                            .placeholder(card.icon().text_color(cx.theme().foreground))
+                            .bg(cx.theme().blue.opacity(0.40)),
+                    )
+                    .child(div().child(card.label()).text_sm())
+                    .child(div().child(card.content()).text_2xl().font_semibold()),
+                cx.theme().secondary,
+            );
 
             cards.push(card);
         }
@@ -69,67 +69,71 @@ impl DashboardContent {
         let data = DashboardPieCard::data();
         let total_count: f64 = data.iter().map(|x| x.data).sum();
 
-        h_flex()
-            .rounded_xl()
-            .shadow_2xl()
-            .border_1()
-            .border_color(black().opacity(0.40))
-            .bg(cx.theme().secondary)
-            .p_12()
-            .w_full()
-            .justify_between()
-            .items_center()
-            .child(
-                div()
-                    .relative()
-                    .child(
-                        div().absolute().top_1().left_3().w_56().child(
-                            PieChart::new(data.clone())
-                                .value(|d| d.data as f32)
-                                .outer_radius(60.)
-                                .inner_radius(60.)
-                                .pad_angle(25. / 100.)
-                                .color(|d| d.color),
+        custom_card(
+            h_flex()
+                .justify_between()
+                .items_center()
+                .child(
+                    div()
+                        .relative()
+                        .child(
+                            div().absolute().w_20().left_2().child(
+                                v_flex()
+                                    .items_center()
+                                    .justify_center()
+                                    .child(div().child(format!("{total_count}")).text_xs())
+                                    .child(div().child("Employees").text_sm().font_semibold()),
+                            ),
+                        )
+                        .child(
+                            div().absolute().inset_2().w_20().child(
+                                v_flex().items_center().justify_center().child(
+                                    PieChart::new(data.clone())
+                                        .value(|d| d.data as f32)
+                                        .outer_radius(70.)
+                                        .inner_radius(55.)
+                                        .pad_angle(25. / 100.)
+                                        .color(|d| d.color),
+                                ),
+                            ),
                         ),
-                    )
-                    .child(
-                        div().absolute().w_32().top_3().left_5().child(
+                )
+                .child(
+                    v_flex()
+                        .gap_2()
+                        .child(
                             v_flex()
-                                .items_center()
+                                .items_start()
                                 .justify_center()
-                                .child(div().child(format!("{total_count}")).text_xs())
-                                .child(div().child("Employees").text_sm().font_semibold()),
+                                .child(
+                                    div()
+                                        .child(format!(
+                                            "{:.1}%",
+                                            (data[0].data / total_count) * 100.0
+                                        ))
+                                        .text_sm()
+                                        .font_thin(),
+                                )
+                                .child(div().child(data[0].label.clone()).text_lg().font_bold()),
+                        )
+                        .child(
+                            v_flex()
+                                .items_start()
+                                .justify_center()
+                                .child(
+                                    div()
+                                        .child(format!(
+                                            "{:.1}%",
+                                            (data[1].data / total_count) * 100.0
+                                        ))
+                                        .text_sm()
+                                        .font_thin(),
+                                )
+                                .child(div().child(data[1].label.clone()).text_lg().font_bold()),
                         ),
-                    ),
-            )
-            .child(
-                v_flex()
-                    .gap_2()
-                    .child(
-                        v_flex()
-                            .items_start()
-                            .justify_center()
-                            .child(
-                                div()
-                                    .child(format!("{:.1}%", (data[0].data / total_count) * 100.0))
-                                    .text_sm()
-                                    .font_thin(),
-                            )
-                            .child(div().child(data[0].label.clone()).text_lg().font_bold()),
-                    )
-                    .child(
-                        v_flex()
-                            .items_start()
-                            .justify_center()
-                            .child(
-                                div()
-                                    .child(format!("{:.1}%", (data[1].data / total_count) * 100.0))
-                                    .text_sm()
-                                    .font_thin(),
-                            )
-                            .child(div().child(data[1].label.clone()).text_lg().font_bold()),
-                    ),
-            )
+                ),
+            cx.theme().secondary,
+        )
     }
 
     fn render_attendance_overview(
@@ -139,82 +143,100 @@ impl DashboardContent {
     ) -> impl IntoElement {
         let data = AttendanceOverviewData::all_data();
 
-        v_flex()
-            .bg(cx.theme().secondary)
-            .p_12()
-            .gap_3()
-            .border_1()
-            .border_color(black().opacity(0.40))
-            .rounded_xl()
-            .shadow_2xl()
-            .h_full()
-            .max_w(px(900.0))
-            .flex_1()
-            .child(
-                h_flex()
-                    .justify_between()
-                    .child(Label::new("Attendance Overview").text_lg().font_bold())
-                    .child(
-                        h_flex().child(
-                            RadioGroup::horizontal("filter-attendance")
-                                .children(["On Time", "Late Arrival", "Absent"])
-                                .selected_index(self.filter_index)
-                                .on_click(cx.listener(|this, index, _, cx| {
-                                    match index {
-                                        0 => {
-                                            this.filter_type =
-                                                Some(AttendanceOverviewFilter::OnTime);
-                                            this.filter_index = Some(*index);
+        custom_card(
+            v_flex()
+                .h_full()
+                .min_w(px(900.0))
+                .flex_1()
+                .child(
+                    h_flex()
+                        .justify_between()
+                        .child(Label::new("Attendance Overview").text_lg().font_bold())
+                        .child(
+                            h_flex().child(
+                                RadioGroup::horizontal("filter-attendance")
+                                    .children(["On Time", "Late Arrival", "Absent"])
+                                    .selected_index(self.filter_index)
+                                    .on_click(cx.listener(|this, index, _, cx| {
+                                        match index {
+                                            0 => {
+                                                this.filter_type =
+                                                    Some(AttendanceOverviewFilter::OnTime);
+                                                this.filter_index = Some(*index);
+                                            }
+                                            1 => {
+                                                this.filter_type =
+                                                    Some(AttendanceOverviewFilter::LateArrival);
+                                                this.filter_index = Some(*index);
+                                            }
+                                            2 => {
+                                                this.filter_type =
+                                                    Some(AttendanceOverviewFilter::Absent);
+                                                this.filter_index = Some(*index);
+                                            }
+                                            _ => {
+                                                this.filter_type =
+                                                    Some(AttendanceOverviewFilter::OnTime);
+                                                this.filter_index = Some(*index);
+                                            }
                                         }
-                                        1 => {
-                                            this.filter_type =
-                                                Some(AttendanceOverviewFilter::LateArrival);
-                                            this.filter_index = Some(*index);
-                                        }
-                                        2 => {
-                                            this.filter_type =
-                                                Some(AttendanceOverviewFilter::Absent);
-                                            this.filter_index = Some(*index);
-                                        }
-                                        _ => {
-                                            this.filter_type =
-                                                Some(AttendanceOverviewFilter::OnTime);
-                                            this.filter_index = Some(*index);
-                                        }
-                                    }
-                                    cx.notify();
-                                })),
+                                        cx.notify();
+                                    })),
+                            ),
                         ),
+                )
+                .child(
+                    h_flex()
+                        .gap_2()
+                        .child(
+                            h_flex()
+                                .gap_1()
+                                .child(div().h_2().w_2().rounded_full().bg(cx.theme().green))
+                                .child(div().text_xs().child("On Time")),
+                        )
+                        .child(
+                            h_flex()
+                                .gap_1()
+                                .child(div().h_2().w_2().rounded_full().bg(cx.theme().yellow))
+                                .child(div().text_xs().child("Late Arrival")),
+                        )
+                        .child(
+                            h_flex()
+                                .gap_1()
+                                .child(div().h_2().w_2().rounded_full().bg(cx.theme().red))
+                                .child(div().text_xs().child("Absent")),
+                        ),
+                )
+                .child(
+                    div().flex_1().py_4().child(
+                        AreaChart::new(data)
+                            .x(|d| d.month.clone())
+                            .y(|d| d.on_time)
+                            .stroke(cx.theme().green)
+                            .fill(linear_gradient(
+                                0.,
+                                linear_color_stop(cx.theme().green.opacity(0.4), 1.),
+                                linear_color_stop(cx.theme().background.opacity(0.3), 0.),
+                            ))
+                            .y(|d| d.late_arrival)
+                            .stroke(cx.theme().yellow)
+                            .fill(linear_gradient(
+                                0.,
+                                linear_color_stop(cx.theme().yellow, 1.),
+                                linear_color_stop(cx.theme().background.opacity(0.3), 0.),
+                            ))
+                            .y(|d| d.absent)
+                            .stroke(cx.theme().red)
+                            .fill(linear_gradient(
+                                0.,
+                                linear_color_stop(cx.theme().red, 1.),
+                                linear_color_stop(cx.theme().background.opacity(0.3), 0.),
+                            ))
+                            .linear(),
                     ),
-            )
-            .child(
-                div().flex_1().py_4().child(
-                    AreaChart::new(data)
-                        .x(|d| d.month.clone())
-                        .y(|d| d.on_time)
-                        .stroke(cx.theme().chart_5)
-                        .fill(linear_gradient(
-                            0.,
-                            linear_color_stop(cx.theme().chart_5.opacity(0.4), 1.),
-                            linear_color_stop(cx.theme().background.opacity(0.3), 0.),
-                        ))
-                        .y(|d| d.late_arrival)
-                        .stroke(cx.theme().chart_2)
-                        .fill(linear_gradient(
-                            0.,
-                            linear_color_stop(cx.theme().chart_2, 1.),
-                            linear_color_stop(cx.theme().background.opacity(0.3), 0.),
-                        ))
-                        .y(|d| d.absent)
-                        .stroke(cx.theme().chart_3)
-                        .fill(linear_gradient(
-                            0.,
-                            linear_color_stop(cx.theme().chart_3, 1.),
-                            linear_color_stop(cx.theme().background.opacity(0.3), 0.),
-                        ))
-                        .linear(),
                 ),
-            )
+            cx.theme().secondary,
+        )
     }
 
     fn render_events_news(&self, _window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
@@ -248,17 +270,13 @@ impl DashboardContent {
             events.push(event_div);
         }
 
-        v_flex()
-            .w(px(900.0))
-            .bg(cx.theme().secondary)
-            .p_12()
-            .gap_3()
-            .border_1()
-            .border_color(black().opacity(0.40))
-            .rounded_xl()
-            .shadow_2xl()
-            .child(div().child("News & Events").text_xl().font_bold())
-            .child(div().grid().grid_cols(2).gap_8().children(events))
+        custom_card(
+            v_flex()
+                .min_w(px(900.0))
+                .child(div().child("News & Events").text_xl().font_bold())
+                .child(div().grid().grid_cols(2).gap_8().children(events)),
+            cx.theme().secondary,
+        )
     }
 }
 
