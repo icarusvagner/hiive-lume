07ea56ccd5ecd5adc7cc3702236384c563683841
icarusvagner <icarusvagner@gmail.com>
2025-12-04 11:37:19 +0800

    removed directories from migrations directory

--- migrations/2025-11-24-025236-0000_create_authentication_tbl/down.sql
+++
@@ -1,7 +0,0 @@
--- This file should undo anything in `up.sql`
-DROP TABLE IF EXISTS tbl_user_account;
-DROP TABLE IF EXISTS tbl_role;
-DROP TABLE IF EXISTS tbl_permission;
-DROP TABLE IF EXISTS tbl_user_role;
-DROP TABLE IF EXISTS tbl_role_permission;
-
